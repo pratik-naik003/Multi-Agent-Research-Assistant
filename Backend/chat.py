3087012ddd4764805351d02f8993665310a7be1e
pratik naik <p0241naik@gmail.com>
2026-01-04 13:45:13 +0530

    Add necessary imports for chat functionality
    Added imports for StateGraph, PromptTemplate, and TypedDict.

--- Backend/chat.py
+++ Backend/chat.py
@@ -1,5 +1,22 @@
 from langchain_cerebras import ChatCerebras
 from dotenv import load_dotenv
-
+from langgraph.graph import StateGraph ,START,END
+from langchain_core.prompts import PromptTemplate
+from typing import TypedDict
 load_dotenv()
+
+
+#Research agent → gathers info
+
+
+
+
+
+
+#Summarizer agent → condenses
+
+
+
+
+#Fact-checker agent → validates
 
